ad8062e80562e57a2b146e6fe6f9cdfaa62fe89d
Sapsay Nikita <simonpatrikoff1@outlook.com>
2023-10-12 20:09:10 +1000

    all at once

--- joystickTest/src/main.cpp
+++ joystickTest/src/main.cpp
@@ -6,183 +6,211 @@
 #include <cstdlib>
 #include <vector>
 #include <unistd.h>
-void halt();
-void sync();
+void sync(uint8_t);
+void handleEvents(SDL_Event);
+void printControls();
+void desync(uint8_t);
+void spin(SDL_Event);
 std::shared_ptr<std::vector<int16_t>> getRightVector(std::list<std::pair<std::vector<int16_t>, uint8_t>>, uint8_t);
-std::list<SDL_Joystick*> joy_ptr_lst; 
+std::vector<SDL_Joystick*> joy_ptr_vctr; 
 int16_t wtf = 0;
 uint8_t count = 0;
 bool curseSet = 0;//fuck you 
+//SDL_Event event;
+int8_t commandPrint_g = -1;
+int8_t idPrint_g = -1;
+bool alarm_f = 0;
+bool something_f = 0;
+bool stupidCunt_f = 0;
+bool stupidCunt2_f = 0;
+bool exit_f = 0;
+std::vector<std::vector<int16_t>> initialState_vctr;
+std::vector<std::vector<int16_t>> axesValues_vctr;
+std::vector<std::vector<int16_t>> buttonsValues_vctr;
 
-std::list<std::pair<std::vector<int16_t>, uint8_t>> initialState_vctr;
-std::list<std::pair<std::vector<int16_t>, uint8_t>> axesValues_vctr;
-std::list<std::pair<std::vector<int16_t>, uint8_t>> buttonsValues_vctr;
- 
 int main(int argc, char **argv)
 {
-  
   SDL_InitSubSystem(SDL_INIT_JOYSTICK);
   SDL_Event event;
-  bool exit = 0;
-  uint8_t cursedAxesCount = 0;
-  halt();
-  sync();
-  if(SDL_NumJoysticks() > 0) 
-  {
-      if(!exit)
-      {
-	while(SDL_PollEvent(&event) >= 0)
-	{
-	  //	  std::cout << "wtf?";
-	  usleep(5000);
-	  std::system("clear"); 
-	  switch(event.type)
-	  {
-	    uint8_t id;
-	    case(SDL_QUIT):
-	      exit = 1;
-	      break;
-	    case(SDL_JOYAXISMOTION):
-	      id = event.jaxis.which;
-	      getRightVector(axesValues_vctr, id)->at(event.jaxis.axis) = event.jaxis.value - getRightVector(initialState_vctr, id)->at(event.jaxis.axis);
-	      break;
-	    case(SDL_JOYBUTTONUP):
-	      id = event.button.which;
-	      getRightVector(buttonsValues_vctr, id)->at(event.jbutton.button) = 0;
-	      break;
-	    case(SDL_JOYBUTTONDOWN):
-	      id = event.button.which;
-	      getRightVector(buttonsValues_vctr, id)->at(event.jbutton.button) = 1;
-	      break;
-	    case(SDL_JOYDEVICEREMOVED):
-	      id = event.jdevice.which;
-	      SDL_Joystick* ptr;
-	      for(auto it : joy_ptr_lst)
-	      {
-		if(SDL_JoystickInstanceID(it) == id)	
-		{
-		  ptr = it;
-		  break;
-		}
-	      }
-	      if(SDL_JoystickGetAttached(ptr))	SDL_JoystickClose(ptr);
-	      halt();
-	      sync();
-	      break;
-	    dafault:
-	      std::cout <<"wtf?\n";
-	      break;
-	  }
-	  if(exit) break;
-
-    	for(auto it : joy_ptr_lst)
-	{
-	  uint8_t id = SDL_JoystickInstanceID(it);
-	  printf("[+] Opened Joystick %d\n", id);
-	  printf("Name: %s\n", SDL_JoystickNameForIndex(0));
-	  printf("Number of Axes: %d\n", SDL_JoystickNumAxes(it));
-	  printf("Number of Buttons: %d\n", SDL_JoystickNumButtons(it));
-	  printf("Number of Balls: %d\n", SDL_JoystickNumBalls(it));
-
-	 	  std::cout << "Buttons: \n";
-	  for(uint8_t i = 0; i < SDL_JoystickNumButtons(it); ++i)
-	  {
-	    if(SDL_JoystickGetButton(it, int(i)) != 0 || 1)
-	    {
-	      std::cout << int(i) << ": ";
-	      std::cout << (int)getRightVector(buttonsValues_vctr, id)->at(i);
-	      std::cout << "\n";
-	    }
-	  }
-	  std::cout << '\n';
-	  std::cout << "Axes: \n";
-	  for(uint8_t i = 0; i < SDL_JoystickNumAxes(it); ++i)
-	  {
-	    if(SDL_JoystickGetAxis(it, int(i)) != 0 || 1)
-	    {
-	      std::cout << int(i) << ": ";
-	      std::cout << (int)getRightVector(axesValues_vctr, id)->at(i);
-	      std::cout << "\n";
-	    }
-	  }	
-	}
-      }
-    }
-
-    }
-
-  else 
-  {
-    printf("[-] Joystick is not detected\n");
-    return -2;
-  }
+  spin(event);
   return 0;
 }
-void halt()
+
+void sync(uint8_t id)
 {
- for(;;)
-  {
-    SDL_JoystickUpdate();
-    count = SDL_NumJoysticks();
-    if(count > 0)
-    {
-      std::cout << "[+] Joytick found\n";
-      break;
-    }
-    else std::cout << "[-] Joystick is not connected\n";
-    sleep(1);
-//    break;
-  }
+  SDL_Joystick *it = SDL_JoystickOpen(id);
+
+  joy_ptr_vctr.push_back(it);
+  initialState_vctr.push_back(std::vector<int16_t>());
+  axesValues_vctr.push_back(std::vector<int16_t>());
+  buttonsValues_vctr.push_back(std::vector<int16_t>());
+  axesValues_vctr[id].resize(SDL_JoystickNumAxes(it));
+  buttonsValues_vctr[id].resize(SDL_JoystickNumButtons(it));
+  initialState_vctr[id].resize(SDL_JoystickNumAxes(it));
+
+
+//  getRightVector(axesValues_vctr, id)->resize(SDL_JoystickNumAxes(joy_ptr), 0);
+//  getRightVector(buttonsValues_vctr, id)->resize(SDL_JoystickNumButtons(joy_ptr), 0);
 }
-void sync()
-{
-  for(uint8_t i = 0; i < count; ++i)	joy_ptr_lst.push_back(SDL_JoystickOpen(i));
-  for(SDL_Joystick *it : joy_ptr_lst)
-  {
-    if(it) 
-    {
-      uint8_t id = SDL_JoystickInstanceID(it);
-      printf("[+] Opened Joystick %d\n", id);
-      printf("Name: %s\n", SDL_JoystickNameForIndex(0));
-      printf("Number of Axes: %d\n", SDL_JoystickNumAxes(it));
-      printf("Number of Buttons: %d\n", SDL_JoystickNumButtons(it));
-      printf("Number of Balls: %d\n", SDL_JoystickNumBalls(it));
-      std::cout << "Axes initial states: \n";
-//      std::pair<std::vector<int16_t>, uint8_t> pair;
-//      pair.first = std::vector<int16_t>(SDL_JoystickNumAxes(it));
-//      pair.second = id;
-//      initialState_vctr.push_back(pair);
-//      pair.fist =
-      initialState_vctr.push_back(std::pair<std::vector<int16_t>, uint8_t>(std::vector<int16_t>(SDL_JoystickNumAxes(it)), id));
 
-      axesValues_vctr.push_back(std::pair<std::vector<int16_t>, uint8_t>(std::vector<int16_t>(SDL_JoystickNumAxes(it)), id));
-      buttonsValues_vctr.push_back(std::pair<std::vector<int16_t>, uint8_t>(std::vector<int16_t>(SDL_JoystickNumButtons(it)), id));
-
-
-      for(uint8_t i = 0; i < SDL_JoystickNumAxes(it); ++i)
-      {
-	if(SDL_JoystickGetAxis(it, int(i)) != 0 || 1)
-	{
-	  std::cout << int(i) << ": ";
-	  bool cursedAxis = SDL_JoystickGetAxisInitialState(it, i, &wtf);
-	  if(!curseSet || 1)	getRightVector(initialState_vctr, id)->push_back(wtf);
-	  std::cout << cursedAxis << " ";
-	  if(cursedAxis)
-	  {
-	    std::cout << wtf;
-	  }
-	  std::cout << "\n";
-	}
-      }
-      getRightVector(axesValues_vctr, id)->resize(SDL_JoystickNumAxes(it), 0);
-      getRightVector(buttonsValues_vctr, id)->resize(SDL_JoystickNumButtons(it), 0);
-    }
-  }
-}
 std::shared_ptr<std::vector<int16_t>> getRightVector(std::list<std::pair<std::vector<int16_t>, uint8_t>> struct_, uint8_t id)
 {
   for(auto it : struct_)
   {
-    if(it.second == id)	return std::make_shared<std::vector<int16_t>>(it.first);
+    std::cout << "id: " << uint16_t(it.second) << '\n';
+    if(it.second == id)
+    {
+      std::cout << "nice?\n";
+      return std::make_shared<std::vector<int16_t>>(it.first);
+    }
+  }
+  std::cout << "not nice\n";
+  return std::make_shared<std::vector<int16_t>>(struct_.front().first);
+}
+
+void desync(uint8_t id)
+{
+  joy_ptr_vctr.erase(joy_ptr_vctr.begin() + id );
+  axesValues_vctr.erase(axesValues_vctr.begin() + id);
+  buttonsValues_vctr.erase(buttonsValues_vctr.begin() + id);
+
+
+
+}
+
+void spin(SDL_Event event)
+{
+//    SDL_JoystickUpdate();
+//    std::cout << "starting iteration in spin()\n";
+  for(;;)
+  {
+    if(exit_f)	return;
+    usleep(5000);
+    handleEvents(event);
+    printControls();
   }
 }
+void handleEvents(SDL_Event event)
+{
+//  SDL_JoystickUpdate();
+//  std::cout << "Handling events?\n";
+//  SDL_Event event;
+while(SDL_PollEvent(&event) > 0)
+{
+  switch(event.type)
+  {
+    uint8_t id;
+    case(SDL_JOYDEVICEADDED):
+      id = event.jdevice.which;//print command if received
+      commandPrint_g = 1;
+      idPrint_g = id;
+      alarm_f = 1;
+      something_f = 1;
+      std::cout << "[!] Joystick detected: " << id << '\n';
+      sync(id);
+      break;
+    case(SDL_QUIT):
+      std::cout << "[!] SDL_QUIT received: \n";//for destructor
+      exit_f = 1;
+      break;
+    case(SDL_JOYAXISMOTION):
+      {
+	id = event.jaxis.which;
+	uint8_t index = event.jaxis.axis;
+	if(axesValues_vctr.at(id).size() > index) axesValues_vctr.at(id).at(index) = event.jaxis.value;
+	else	stupidCunt_f = 1;
+	break;
+      }
+    case(SDL_JOYBUTTONUP):
+      {
+	
+	id = event.jbutton.which;
+	uint8_t index = event.jbutton.button;
+	if(buttonsValues_vctr.at(id).size() > index) buttonsValues_vctr.at(id).at(index) = 0;
+	else	stupidCunt_f = 1;
+	break;
+      }
+    case(SDL_JOYBUTTONDOWN):
+      {
+	std::cout << "[!] Button down\n";
+	id = event.jbutton.which;
+	uint8_t index = event.jbutton.button;
+	if(buttonsValues_vctr.at(id).size() > index) buttonsValues_vctr.at(id).at(index) = 1;
+	else	stupidCunt_f = 0;
+	break;
+      }
+   case(SDL_JOYDEVICEREMOVED):
+      if(SDL_NumJoysticks() == 0)
+      {
+	something_f = 0;
+	alarm_f = 0;
+      }
+      id = event.jdevice.which;
+      desync(id);
+      break;
+    dafault:
+      std::cout <<"wtf?\n";
+      break;
+  }
+}
+}
+void printControls()
+{
+  if(!something_f && !alarm_f)
+  {
+    std::system("clear");
+    std::cout << "[~] No joysticks attached\n";
+    alarm_f = 1;
+  }
+  if(!something_f)  return;
+  std::system("clear");
+//  for(SDL_Joystick *it : joy_ptr_lst)
+  for(uint8_t i = 0; i < joy_ptr_vctr.size(); ++i)
+  {
+    if(i)  std::cout << "\n\n";
+    SDL_Joystick* it = joy_ptr_vctr.at(i);
+    uint8_t id = SDL_JoystickInstanceID(it);
+    printf("[+] Opened Joystick %d\n", id);
+    printf("Name: %s\n", SDL_JoystickNameForIndex(i));
+    printf("Number of Axes: %d\n", SDL_JoystickNumAxes(it));
+    printf("Number of Buttons: %d\n", SDL_JoystickNumButtons(it));
+    printf("Number of Balls: %d\n", SDL_JoystickNumBalls(it));
+    std::cout << "Size of joystick vector: " << joy_ptr_vctr.size() << '\n';
+    std::cout << "Size of axes list: " << buttonsValues_vctr.at(i).size() << '\n';
+//    std::cout << "Size of it's vector: " << buttonsValues_vctr.front().first.size() << '\n';
+    std::cout << "Size of axes vector: " << axesValues_vctr.at(i).size() << '\n'; 
+    std::cout << "Size of buttons vector: " << buttonsValues_vctr.at(i).size() << '\n'; 
+//    std::cout << "Are it the same vectors: " << buttonsValues_vctr, id).get() == &(buttonsValues_vctr.front().first)) << '\n';
+    if(stupidCunt_f) std::cout << "Go fuck yourself\n"; 
+    if(stupidCunt2_f)	std::cout << "ok, thats something\n";
+    printf("Initial states:\n");
+    for(uint8_t j = 0; j < SDL_JoystickNumAxes(it); ++j)
+    {
+      std::cout << int(j) << ": ";
+      bool cursedAxis = SDL_JoystickGetAxisInitialState(it, j, &wtf);
+      initialState_vctr.at(i).at(j) = wtf;//push_back(wtf);
+      std::cout << cursedAxis << " " << initialState_vctr.at(i).at(j) << '\n';
+    }
+    std::cout << "Buttons: \n";
+    for(uint8_t j = 0; j < SDL_JoystickNumButtons(it); ++j)
+    {
+      if(SDL_JoystickGetButton(it, int(j)) != 0 || 1)
+      {
+	std::cout << int(j) << ": ";
+	std::cout << buttonsValues_vctr[i][j];
+	std::cout << "\n";
+      }
+    }
+    std::cout << "Axes: \n";
+    for(uint8_t j = 0; j < SDL_JoystickNumAxes(it); ++j)
+    {
+      if(SDL_JoystickGetAxis(it, int(j)) != 0 || 1)
+      {
+	std::cout << int(j) << ": ";
+	std::cout << axesValues_vctr[i][j];
+	std::cout << "\n";
+      }
+    }
+  }
+}
+  
