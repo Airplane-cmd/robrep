44124fbbbcfb69121a247e4e763fdbeee0e6832e
Sapsay Nikita <simonpatrikoff1@outlook.com>
2023-10-26 21:46:53 +1000

    fucking finally

---
+++ ServerClient/build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/home/bthd/WS/Networking/ServerClient/src/SimpleClient.cpp.o"
+  "CMakeFiles/client.dir/home/bthd/WS/Networking/ServerClient/src/SimpleClient.cpp.o.d"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ServerClient/build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/home/bthd/WS/Networking/ServerClient/src/SimpleServer.cpp.o"
+  "CMakeFiles/server.dir/home/bthd/WS/Networking/ServerClient/src/SimpleServer.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
